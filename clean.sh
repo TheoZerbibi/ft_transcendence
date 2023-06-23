d7fc29bcd92aee777fd28aa6b3cea3389fa0d733
Theo ZERIBI <tzeribi@gmail.com>
2023-06-23 05:21:49 +0200

    🔥 chore(Dockerfile): remove back/Dockerfile as it is no longer needed ✨ feat(prisma): add Dockerfile, package.json and tsconfig.json to run prisma in a container 🐛 fix(api): run prisma migrate dev command on api container start to apply database schema changes 🔨 refactor(api): add prisma schema generation to build script to avoid manual generation on development machines 🔨 refactor(api): add prisma schema generation to prisma container start script to avoid manual generation on container start 🔨 refactor(api): add prisma schema generation to prisma container start script to avoid manual generation on container start 🔨 refactor(clean.sh): add cleaning of .pnpm-store/ folder to clean.sh script

--- clean.sh
+++ clean.sh
@@ -4,3 +4,6 @@ docker-compose down --rmi all --volumes
 docker rm -f $(docker ps -a -q)
 docker system prune -af
 docker volume rm $(docker volume ls -q)
+
+echo "Cleaning node_modules and dist folders"
+rm -rf ./**/dist ./**/node_modules ./**/.pnpm-store/ ./**/dist/
