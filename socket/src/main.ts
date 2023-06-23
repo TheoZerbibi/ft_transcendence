65b8639b819b71a6a8c189300a15e871efb9c2c6
Theo ZERIBI <tzeribi@gmail.com>
2023-06-23 08:32:22 +0200

    🔧 chore(socket): add eslint, prettier, jest, and nest-cli configuration files ✨ feat(socket): add RedisIoAdapter to enable socket.io to use Redis as an adapter ✨ feat(socket): add AppController and AppService to handle HTTP requests ✨ feat(socket): add Jest e2e test for AppController ✨ feat(socket): add html file to test socket.io connection ✨ feat(socket): add Makefile to run lint, format, and tests using docker-compose

---
+++ socket/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { RedisIoAdapter } from './adapters/redis-io.adapter';
+import * as dotenv from 'dotenv';
+
+async function bootstrap() {
+  dotenv.config({ path: '/.env' });
+
+  const app = await NestFactory.create(AppModule);
+
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+  app.useWebSocketAdapter(redisIoAdapter);
+
+  await app.listen(4000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
