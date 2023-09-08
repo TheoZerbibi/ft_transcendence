095a0c3a58b8d2c62ff167f53b47687469490ebd
Theo ZERIBI <tzeribi@gmail.com>
2023-09-08 15:54:34 +0200

    Fix build in front

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,15 +3,23 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue({
-            template: {
-                compilerOptions: {
-                    isCustomElement: (tag) => {
-                        return tag.startsWith('ion-'); // (return true)
-                    },
-                },
-            },
-        }),
-    ],
+	plugins: [
+		vue({
+			template: {
+				compilerOptions: {
+					isCustomElement: (tag) => {
+						return tag.startsWith('ion-'); // (return true)
+					},
+				},
+			},
+		}),
+		
+	],
+	css: {
+		preprocessorOptions: {
+		 	scss: {
+				includePaths: ["node_modules"],
+			},
+		},
+	},
 });
