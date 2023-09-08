358e2a26444cca0abab9bfa230e64798c9575964
Theo ZERIBI <tzeribi@gmail.com>
2023-09-08 16:05:14 +0200

    Reset pnpm-lock

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,23 +3,22 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue({
-			template: {
-				compilerOptions: {
-					isCustomElement: (tag) => {
-						return tag.startsWith('ion-'); // (return true)
-					},
-				},
-			},
-		}),
-		
-	],
-	css: {
-		preprocessorOptions: {
-		 	scss: {
-				includePaths: ["node_modules"],
-			},
-		},
-	},
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    isCustomElement: (tag) => {
+                        return tag.startsWith('ion-'); // (return true)
+                    },
+                },
+            },
+        }),
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                includePaths: ['node_modules'],
+            },
+        },
+    },
 });
--- front/vitest.config.ts
+++ front/vitest.config.ts
@@ -11,28 +11,28 @@ import viteConfig from './vite.config';
  * @see {@link https://vitest.dev/config/}
  */
 export default mergeConfig(
-	viteConfig,
-	defineConfig({
-		plugins: [vue()],
-		// Resolver
-		resolve: {
-			// https://vitest.dev/config/#alias
-			alias: {
-				'@': fileURLToPath(new URL('./src', import.meta.url)),
-				'~': fileURLToPath(new URL('./node_modules', import.meta.url)),
-			},
-			extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
-		},
-		test: {
-			environment: 'jsdom',
-			exclude: [...configDefaults.exclude, 'e2e/*'],
-			root: fileURLToPath(new URL('./', import.meta.url)),
-			// https://vuetifyjs.com/en/getting-started/unit-testing/
-			server: {
-				deps: {
-					inline: ['vuetify'],
-				},
-			},
-		},
-	}),
+    viteConfig,
+    defineConfig({
+        plugins: [vue()],
+        // Resolver
+        resolve: {
+            // https://vitest.dev/config/#alias
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+                '~': fileURLToPath(new URL('./node_modules', import.meta.url)),
+            },
+            extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+        },
+        test: {
+            environment: 'jsdom',
+            exclude: [...configDefaults.exclude, 'e2e/*'],
+            root: fileURLToPath(new URL('./', import.meta.url)),
+            // https://vuetifyjs.com/en/getting-started/unit-testing/
+            server: {
+                deps: {
+                    inline: ['vuetify'],
+                },
+            },
+        },
+    }),
 );
