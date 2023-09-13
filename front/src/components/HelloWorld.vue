e3901e8aff3f5d4f360d47635bd0d4b003f83456
Kyubie <gnouny@yahoo.fr>
2023-09-13 13:29:23 +0200

    wow

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -5,50 +5,50 @@
 				<h1 class="display-2 font-weight-bold mb-3">
 					Welcome to<br> ft_transcendence MEOW team
 				</h1>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-				<v-img :src="logo" class="my-3" contain height="140"/>
-		    <br>
-		    <br>
-			<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<h4>Starring:</h4>
-				<p class="subheading font-weight-regular">
-					Théo
-					<a
-					href="https://profile.intra.42.fr/users/thzeribi"
-					target="_blank"
-					>(thzeribi 42 Intra profile)</a>
-					<br>Ismaël
-					<a
-					href="https://profile.intra.42.fr/users/iguidado"
-					target="_blank"
-					>(iguidado 42 Intra profile)</a>
-					<br>Gaëlle
-					<a
-					href="https://profile.intra.42.fr/users/grannou"
-					target="_blank"
-					>(grannou 42 Intra profile)</a>
-					<br>Semiha
-					<a
-					href="https://profile.intra.42.fr/users/seozcan"
-					target="_blank"
-					>(seozcan 42 Intra profile)</a>
-					<br>Noémi
-					<a
-					href="https://profile.intra.42.fr/users/nfauconn"
-					target="_blank"
-					>(nfauconn 42 Intra profile)</a>
-				</p>
 			</v-col>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
 		</v-row>
+		<v-col>
+			<v-img :src="logo" class="my-3" contain height="140"/>
+		</v-col>    
+		<br>
+		    <br>
+			<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<h4>Starring</h4>
+
+		<!-- Première ligne -->
+		<v-row>
+		    <v-col v-for="(prenom, index) in prenoms" :key="index" cols="2.5">
+		        <!-- Contenu de la colonne -->
+		        <v-card class="transparent-background">
+		          <v-card-text>
+		            {{ prenom }}
+		          </v-card-text>
+		        </v-card>
+		      </v-col>
+		    </v-row>
+		
+		<!-- Deuxième ligne -->
+		<v-row>
+		    <v-col v-for="(qrCode, index) in qrCodesImgPath" :key="index" cols="2.5">
+		        <!-- Contenu de la colonne -->
+		        <v-card class="transparent-background">
+		          <v-card-text>
+		            {{ qrCode }}
+		          </v-card-text>
+		        </v-card>
+		    </v-col>
+		</v-row>
+
 	</v-container>
 </template>
 
@@ -64,24 +64,39 @@ export default defineComponent({
   data () {
 	return {
 	  logo,
+	  prenoms: ['Théo', 'Ismaël', 'Gaëlle', 'Semiha', 'Noémi'],
+      qrCodesImgPath: [
+		'thzeribi',
+		'iguidado',
+		'grannou',
+		'seozcan',
+		'nfauconn']
 	}
   },
 })
 </script>
 
 <style>
-  .backgroundWallpaper{
-  	background-image: url('/src/assets/PongWallpaper.jpg');
-    background-size: cover;
-    background-repeat: no-repeat;
-	background-attachment: fixed;
-	background-position: center;
-	background-color: black;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	height: 100vh;
-	color: white;
-}
+	.backgroundWallpaper
+	{
+  		background-image: url('/src/assets/PongWallpaper.jpg');
+    	background-size: cover;
+    	background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-position: center;
+		background-color: black;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		color: white;
+	}
+	.transparent-background
+	{
+		background-color: transparent;
+		color: white;
+		text-align: center;
+
+	}
 </style>
