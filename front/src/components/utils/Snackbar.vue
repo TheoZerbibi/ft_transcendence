192d44f040e1a6655a59d5c1e68b9ba049e208a1
Theo ZERIBI <tzeribi@gmail.com>
2023-09-25 11:58:08 +0200

    Remove useless in Snackbar Component

--- front/src/components/utils/Snackbar.vue
+++ front/src/components/utils/Snackbar.vue
@@ -1,9 +1,4 @@
-<template>
-	<v-snackbar v-model="snackbar" :timeout="timeout" :color="color">
-		{{ text }}
-	</v-snackbar>
-</template>
-
+// eslint-disable-next-line vue/multi-word-component-names
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 
@@ -13,11 +8,8 @@ const snackbarStore = useSnackbarStore();
 const { snackbar, text, timeout, color } = storeToRefs(snackbarStore);
 </script>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-	// eslint-disable-next-line vue/multi-word-component-names
-	name: 'Snackbar',
-});
-</script>
+<template>
+	<v-snackbar v-model="snackbar" :timeout="timeout" :color="color">
+		{{ text }}
+	</v-snackbar>
+</template>
