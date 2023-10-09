72bd4d366946a720f79f80fd887373dfccdb932a
Ismael GUIDADOU <iguidado@bess-f2r5s5.clusters.42paris.fr>
2023-10-09 21:52:17 +0200

    merged with latest

--- back/src/channel/channel.service.ts
+++ back/src/channel/channel.service.ts
@@ -66,17 +66,44 @@ export class ChannelService {
 		}
 	}
 
+	// async create(dto: CreateChannelDto, userId: number) {
+	// 			try {
+	// 				const	channel = await this.prisma.channel.create({
+	// 					data : {
+	// 						name: dto.name,
+	// 						password: dto.password,
+	// 						public: dto.is_public
+	// 					},
+	// 				})
+	// 				const	channel_user = await this.prisma.channelUser.create({
+	// 					data : {
+	// 						channel_id: channel.id,
+	// 						user_id: userId,
+	// 						is_owner: true
+	// 					}
+	// 				})
+	// 				return channel;
+	// 			} catch (e) {
+	// 				if (e instanceof Prisma.PrismaClientKnownRequestError)
+	// 					{
+	// 						if (e.code === 'P2002')
+	// 							throw new ForbiddenException('Channel name taken');
+	// 					}
+	// 			}
+	// }
+	//
+
+	update(id: number, updateChannelDto: UpdateChannelDto) {
+		console.log(updateChannelDto);
+		return `This action updates a #${id} channel`;
+	}
+
 	findAll() {
 		return `This action returns all channel`;
 	}
 
 	findOne(id: number) {
 		return `This action returns a #${id} channel`;
-	}
-
-	update(id: number, updateChannelDto: UpdateChannelDto) {
-		console.log(updateChannelDto);
-		return `This action updates a #${id} channel`;
 	}
 
 	remove(id: number) {
