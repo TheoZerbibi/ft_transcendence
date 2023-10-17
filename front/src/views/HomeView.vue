3da2b94c8b660bee57af5d7394c5c3545f736ece
Kyubie <gnouny@yahoo.fr>
2023-10-17 09:25:30 +0200

    portfolio to fix

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,78 +1,203 @@
 <template>
 	<v-app id="home">
-		<NavBar>
-			<v-container fluid>
-				<div class="head">
+		<NavBa />
+		<v-container fluid>
+			<div class="head">
+				<v-row>
+					<v-col cols="5">
+						<div style="position: relative" clas="mt-16">
+							<h1 class="text-grey">Hello,</h1>
+							<h1 class="text-white">Welcome to ft_transcendence of</h1>
+							<span class="text-grey">Théo, Gaëlle, Semiha, Noémie and Norminet</span><br />
+							<v-btn title dark class="text-yellow mt-8" variant="outlined">Contact us</v-btn>
+						</div>
+					</v-col>
+					<v-col cols="2">
+						<div style="position: absolute;z-index: 9999;bottom: 0;margin-left: auto;margin-right: 0;left: 0;right: 0;text-align: center;">
+							<v-icon>fas fa angle-double-down</v-icon>
+						</div>
+					</v-col>
+					<v-col cols="5">
+						<div style="position: relattive;z-index: 9999;" class="mt-16">
+							<v-img src="Cats.jpg" contain max-height="300"></v-img>
+						</div>
+					</v-col>
+				</v-row>
+			</div>
+			<v-col cols="12" class="mt-16" id="about">
+				<div>
 					<v-row>
-						<v-col cols="5">
-							<div style="position: relative" clas="mt-16">
-								<h1 class="text-grey">Hello,</h1>
-								<h1 class="text-white">Welcome to ft_transcendence of</h1>
-								<span class="text-grey">Théo, Gaëlle, Semiha, Noémie and Norminet</span><br />
-								<v-btn title dark class="text-yellow mt-8" variant="outlined">Contact us</v-btn>
+						<v-col cols="12" sm="6">
+							<div class="egg">
+								<v-img src="OmoriWallpaper00.jpg" max-height="300"></v-img>
 							</div>
 						</v-col>
-						<v-col cols="2">
-							<div style="position: absolute;z-index: 9999;bottom: 0;margin-left: auto;margin-right: 0;left: 0;right: 0;text-align: center;">
-								<v-icon>fas fa angle-double-down</v-icon>
+						<v-col cols="12" sm="6">
+							<h5 class="mt-16">About us</h5>
+							<div style="width: 120px">
+								<v-slider v-model="slider2" color="yellow" label="track-color">
+								</v-slider>
 							</div>
-						</v-col>
-						<v-col cols="5">
-							<div style="position: relattive;z-index: 9999;" class="mt-16">
-								<v-img src="Cats.jpg" contain max-height="300"></v-img>
-							</div>
+							<h4 class="mt-n4">We are fans of Norminet</h4>
+							<h4>and Omori world</h4>
+							<p class="text-grey">
+								Vous savez, je ne crois pas qu'il y ait de bonne ou de mauvaise situation...
+							</p>
+							<br />
+							<p class="text-grey">
+								Scribe c'est bien comme situation?
+							</p>
+							<v-btn tile dark color="yellow" class="mt-4">
+								Download a Norminet picture
+							</v-btn>
 						</v-col>
 					</v-row>
 				</div>
-				<v-col cols="12" class="mt-16" id="about">
-					<div>
-						<v-row>
-							<v-col cols="12" sm="6">
-								<div class="egg">
-									<v-img src="OmoriWallpaper00.jpg" max-height="300"></v-img>
-								</div>
-							</v-col>
-							<v-col cols="12" sm="6">
-								<h5 class="mt-16">About us</h5>
-								<div style="width: 120px">
-									<v-slider v-model="slider2" color="yellow" label="track-color">
-									</v-slider>
-								</div>
-								<h4 class="mt-n4">We are fans of Norminet</h4>
-								<h4>and Omori world</h4>
-								<p class="text-grey">
-									Vous savez, je ne crois pas qu'il y ait de bonne ou de mauvaise situation...
-								</p>
-								<br />
-								<p class="text-grey">
-									Scribe c'est bien comme situation?
-								</p>
-								<v-btn tile dark color="yellow" class="mt-4">
-									Download a Norminet picture
-								</v-btn>
-							</v-col>
-						</v-row>
-					</div>
-				</v-col>
-				<div class="text-center mt-4">
-					<h2>What we do</h2>
-					<div style="width: 120px; margin: 0 auto;">
-					<v-slider v-model="slider2" color="yellow" label="track-color"></v-slider>
-					</div>
+			</v-col>
+			<div class="text-center mt-4">
+				<h2>What we do</h2>
+				<div style="width: 120px; margin: 0 auto;">
+				<v-slider v-model="slider2" color="yellow" label="track-color"></v-slider>
 				</div>
-				<v-col cols="12" class="padd" id="portfolio">
-					<div class="first" id="project">
-						<v-row>
-							<v-col cols="12">
-								<div class="child">
-									
-								</div>
+			</div>
+			<v-col cols="12" class="padd" id="portfolio">
+				<div class="first" id="project">
+					<v-row>
+						<v-col cols="12">
+							<div class="child">
+								<v-btn ico="fas fa-laptop" color="#FBDF7E" class="text-white"></v-btn>
+								<h3 class="ml-3 mt-4">Pong game</h3>
+								<p class="text-grey ml-3 mt-4 text-caption">
+									Created by Allan Alcorn<br />released in 1972<br />by Atari
+								</p>
+							</div>
+							<div class="child">
+								<v-btn ico="fas fa-mobile-alt" color="#FBDF7E" class="text-white"></v-btn>
+								<h3 class="ml-3 mt-4">Chat</h3>
+								<p class="text-grey ml-3 mt-4 text-caption">
+									blabla<br />blabla<br />blabla
+								</p>
+							</div>
+							<div class="child">
+								<v-btn ico="fas fa-camera" color="#FBDF7E" class="text-white"></v-btn>
+								<h3 class="ml-3 mt-4">Photography</h3>
+								<p class="text-grey ml-3 mt-4 text-caption">
+									blabla<br />blabla<br />blabla
+								</p>
+							</div>
+						</v-col>
+					</v-row>
+					<v-divider></v-divider>
+				</div>
+			</v-col>
+			<v-col cols="12" sm="12" id="services">
+				<div class="d-flex justify-center mb-6">
+					<v-btn color="#FBDF7E" class="mr-2">All</v-btn>
+					<v-btn class="mr-2" variant="tonal">Norminet</v-btn>
+					<v-btn class="mr-2" variant="tonal">Babache</v-btn>
+					<v-btn class="mr-2" variant="tonal">Moulinet</v-btn>
+					<v-btn variant="tonal">Tylung</v-btn>
+				</div>
+			</v-col>
+			<v-col cols="12" class="imgHover">
+				<v-row class="fill-height" align="center" justify="center">
+					<template v-for="(item, i) in items" :key="i">
+						<v-col cols="12" md="4">
+							<v-hover v-slot="{isHovering, props}">
+								<v-card :elevation="isHovering ? 12: 2" :class="{'on-hover' : isHovering}" v-bind="props">
+									<v-img :src="item.img" height="225px" cover></v-img>
+								</v-card>
+							</v-hover>
+						</v-col>
+					</template> 
+				</v-row>
+			</v-col>
+			<v-col cols="12" sm="12">
+				<div class="d-flex jusify-center mb-6">
+					<v-btn color="#FBDF7E" class="mt-4">Lead More</v-btn>
+				</div>
+			</v-col>
+			<v-col cols="12" id="page">
+				<div class="pre">
+					<v-row>
+						<v-col cols="12" sm="4">
+							<v-card class="mv-auto" max-width="344" hight="">
+								<v-img src="Cats.jpg" height="200px" cover></v-img>
+								<v-card-title>We provide you the best</v-card-title>
+								<v-card-subtitle>
+									By Dream Team | november 30th 2023
+								</v-card-subtitle>
+								<v-card-text>
+									bla bla bla bla bla
+								</v-card-text>
+							</v-card>
+						</v-col>
+					</v-row>
+				</div>
+			</v-col>
+			<v-col cols="12" id="biog">
+				<div class="hire">
+					<v-row>
+						<v-col cols="12" sm="8">
+							<h1 class="mt-9">Hire us for your awesome projects</h1>
+							<p class="text-grey">
+								Bla bla bla bla bla bla bla bla bla
+							</p>
+						</v-col>
+						<v-col cols="12" sm="4">
+							<v-btn color="#FBDF7E" class="mt-15">Hire us</v-btn>
+						</v-col>
+					</v-row>
+				</div>
+			</v-col>
+			<v-col cols="12" sm="12" class="px-16" id="contact">
+				<v-row>
+					<v-col cols="12" sm="4">
+						<div class="child">
+							<h1>Contact info.</h1>
+							<v-btn icon="fas fa-map-marker-alt" class="mt-10" variant="outlined"></v-btn><br />
+							<span class="text-caption">Positive quote</span><br />
+							<v-btn icon="fas fa-phone-alt" class="mt-10" variant="outlined"></v-btn><br />
+							<span class="text-caption">42 42 42</span><br />
+							<span class="text-caption">42 42 42</span><br />
+							<v-btn icon="fas fa-envelope" class="mt-10" variant="outlined"></v-btn><br />
+							<span class="text-caption">hello@akatsuki.com</span><br />
+							<span class="text-caption">hello@akatsuki.com</span><br />
+						</div>
+					</v-col>
+					<v-col cols="12" sm="¨8">
+						<h1 class="mt-8">Send your message</h1>
+						<v-divider></v-divider>
+						<span class="text-caption">
+							Vous savez, moi je ne crois pas qu’il y ait de bonne ou de mauvaise situation.
+							Moi, si je devais résumer ma vie aujourd’hui avec vous, je dirais que c’est d’abord des rencontres.
+							Des gens qui m’ont tendu la main, peut-être à un moment où je ne pouvais pas, où j’étais seul chez moi.
+							Et c’est assez curieux de se dire que les hasards, les rencontres forgent une destinée...
+							Parce que quand on a le goût de la chose, quand on a le goût de la chose bien faite, le beau geste,
+							parfois on ne trouve pas l’interlocuteur en face je dirais, le miroir qui vous aide à avancer.
+							Alors ça n’est pas mon cas, comme je disais là, puisque moi au contraire, j’ai pu ;
+							et je dis merci à la vie, je lui dis merci, je chante la vie, je danse la vie... je ne suis qu’amour !
+							Et finalement, quand des gens me disent « Mais comment fais-tu pour avoir cette humanité ? »,
+							je leur réponds très simplement que c’est ce goût de l’amour,
+							ce goût donc qui m’a poussé aujourd’hui à entreprendre une construction mécanique...
+							mais demain qui sait ?
+							Peut-être simplement à me mettre au service de la communauté, à faire le don, le don de soi.
+						</span>
+						<v-row class="mt-10">
+							<v-col cols="12" sm="6">
+								<v-text-field label="Name" persisten-hint variant="outlined"></v-text-field>
+							</v-col>
+							<v-col cols="12" sm="6">
+								<v-text-field label="Phone No" persisten-hint variant="outlined"></v-text-field>
 							</v-col>
 						</v-row>
-					</div>
-				</v-col>
-			</v-container>
-		</NavBar>
+						<v-textarea label="Message" persisten-hint variant="outlined"></v-textarea>
+						<v-btn color="#FBDF7E" class="mt-2">Submit Now</v-btn>
+					</v-col>
+				</v-row>
+			</v-col>
+		</v-container>
+		<FooterView />
 	</v-app>
 </template>
 
@@ -93,6 +218,15 @@ export default defineComponent({
 				},
 				{
 					img: "OmoriWallpaper00.jpg"
+				},
+				{
+					img: "OmoriWallpaper01.jpg"
+				},
+				{
+					img: "OmoriWallpaper02.jpg"
+				},
+				{
+					img: "OmoriWallpaper03.jpg"
 				},
 			],
 		}
