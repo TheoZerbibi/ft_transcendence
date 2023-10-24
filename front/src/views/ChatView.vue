73fb0cdc991f2b35233194349d977eb1cc8f1fe6
Ismael GUIDADOU <iguidado@made-f0Cr10s4.clusters.42paris.fr>
2023-10-24 06:37:09 +0200

    Added chat front design and template

--- front/src/views/ChatView.vue
+++ front/src/views/ChatView.vue
@@ -1,27 +1,29 @@
 <template>
-<div>
-<h1>Chat App</h1>
+<Discord></Discord>
+<!--
+	<v-expansion-panels>
+	<v-expansion-panel>
+	<v-expansion-panel-header>Chat</v-expansion-panel-header>
+	<v-expansion-panel-content>
 
-<v-expansion-panels>
-<v-expansion-panel>
-<v-expansion-panel-header>Chat</v-expansion-panel-header>
-<v-expansion-panel-content>
-<chat-box></chat-box>
-</v-expansion-panel-content>
-</v-expansion-panel>
-</v-expansion-panels>
-
-</div>
+	</v-expansion-panel-content>
+	</v-expansion-panel>
+	</v-expansion-panels>
+-->
 </template>
 
 <script>
 import Chat from '../components/chat/Chat.vue'; // Adjust the path as needed
+import Discord from '../components/chat/Discord.vue'; // Adjust the path as needed
 import ChatBox from '../components/chat/ChatBox.vue'; // Adjust the path as needed
+import ChatBox1 from '../components/chat/ChatBox1.vue';
 
 export default {
 components: {
 		    Chat,
 		    'chat-box': ChatBox,
+			ChatBox1,
+			Discord,
 	    },
 };
 
