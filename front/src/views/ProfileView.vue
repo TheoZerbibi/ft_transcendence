2e600133c28d324c1beab90332319aabdaff04cc
Kyubie <gnouny@yahoo.fr>
2023-10-29 17:38:27 +0100

    just vuetify icon works

--- front/src/views/ProfileView.vue
+++ front/src/views/ProfileView.vue
@@ -1,8 +1,15 @@
 <template>
-    <h1> This is the profile page</h1>    
-
+    <div>
+        <h1> This is the profile page</h1>    
+        <v-btn class="neonPolice" @click="$router.push('chat')">Chat</v-btn>
+        <v-icon icon="$vuetify"></v-icon>
+    
+    </div>
+    
 </template>
 
-<script></script>
+<script>
+</script>
 
-<style></style>
+<style>
+</style>
