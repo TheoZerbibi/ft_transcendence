e88f990e70c522a3067f650b6e4b1c71695c265b
Kyubie <gnouny@yahoo.fr>
2023-11-06 19:03:49 +0100

    core:front css grid display for profile page

--- front/src/views/ProfileView.vue
+++ front/src/views/ProfileView.vue
@@ -2,22 +2,29 @@
 	<header>
 		<h1 class="neonPolice" color=ẅhite>This is the profile page</h1>
 	</header>
-	<main>
-		<section>
-			<v-container class="profileHome">
-				<v-card>
-					<v-btn class="neonPolice" @click="$router.push('chat')">Chat &#129418;</v-btn>
-				</v-card>
-				<v-card class="insideBox01">Card 1</v-card>
-				<v-card class="insideBox02">Card 2</v-card>
-				<v-card class="insideBox03">Card 3</v-card>
-			</v-container>
-		</section>
-	</main>
 	<nav>
 		<p>This is a navbar to display after authentification, vertically left better?</p>
 		<!--Cf ul and li balises-->
 	</nav>
+	<main>
+		<section>
+			<v-container class="profileHome">
+				<!-- <div class="¨boxZero"> -->
+					<v-btn class="boxZero neonPolice" @click="$router.push('chat')">Chat &#128062;</v-btn>
+				<!-- </div> -->
+				<div class="boxOne">Box 1 &#128001</div>
+				<div class="boxTwo">Box 2 &#128048</div>
+				<div class="boxThree">Box 3 &#129418</div>
+				<div class="boxFour">Box 4 &#128013</div>
+				<div class="boxFive">Box 5 &#128014</div>
+				<div class="boxSix">Box 6 &#128025</div>
+				<div class="boxSeven">Box 7 &#128029</div>
+				<div class="boxEight">Box 8 &#128030</div>
+				<div class="boxNine">Box 9 &#128060</div>
+
+			</v-container>
+		</section>
+	</main>
 	<aside>
 		<p>This is an aside section</p>
 	</aside>
@@ -39,24 +46,60 @@ export default defineComponent({
 
 <style scoped>
 .profileHome{
-	display:flex;
-	flex-direction: row;
-	justify-content: space-around;
-	height: 600px;
+	display:grid;
+	/* grid-template-columns: 250px 250px 250px 250px; */
+	grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+	/* grid-template-rows: 150px 300px; */
+	grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
 	align-items: center;
 	gap: 10px;
 }
-
-.insideBox01 {
-	order: 1;
+.box {
+	margin:auto;
 }
 
-.insideBox02 {
-	order: 2;
+.boxZero {
+	grid-column-start: 1;
+	grid-column-end: 2;
+	grid-row-start: 1;
+	grid-row-end: 6;
+}
+.boxOne {
+	grid-column-start: 1;
+	grid-column-end: 7;
+}
+.boxTwo {
+	grid-column-start: 1;
+	grid-column-end: 2;
+}
+.boxThree {
+	grid-column-start: 2;
+	grid-column-end: 4;
+}
+.boxFour {
+	grid-column-start: 4;
+	grid-column-end: 6;
+}
+.boxFive {
+	grid-column-start: 2;
+	grid-column-end: 6;
+}
+.boxSix {
+	grid-column-start: 1;
+	grid-column-end: 4;
+}
+.boxSeven {
+	grid-column-start: 4;
+	grid-column-end: 6;
+}
+.boxEight {
+	grid-column-start: 1;
+	grid-column-end: 3;
+}
+.boxNine {
+	grid-column-start: 3;
+	grid-column-end: 6;
 }
 
-.insideBox03 {
-	order: 3;
-}
 
 </style>
