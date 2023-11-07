2e82e79635e5503203c08284d8057cfa21e64c28
Kyubie <gnouny@yahoo.fr>
2023-11-07 18:51:35 +0100

    core:feat prototype grid suite profile page

--- front/src/views/ProfileView.vue
+++ front/src/views/ProfileView.vue
@@ -170,19 +170,40 @@ export default defineComponent({
 	</nav>
 	<main>
 		<section>
-			<v-container class="profileHome">
-				<div class="box zero">
+			<v-container class="profileHome neonPolice">
+				<div class="box one">Settings &#128001
+					<v-btn class="neonPolice">About ℹ️</v-btn>
+					<v-btn class="neonPolice">Rules 📄</v-btn>
+					<v-btn class="neonPolice">Help ❓</v-btn>
+				</div>
+				<div class="box two">User infos &#128048
+					<div>Profile picture</div>
+					<div>name 1</div>
+					<div>does not display</div>
+					<div>username</div>
+				</div>
+				<div class="box three">Empty &#129418</div>
+				<div class="box four">Buttons &#128013
+					<v-btn class="neonPolice">Modify 🖋</v-btn>
+					<v-btn class="neonPolice">Logout 🚪</v-btn>
+				</div>
+				<div class="box five">Game stats &#128014
+					<div></div>
+					<div></div>
+				</div>
+				<div class="box six">
 					<v-btn class="chatButtom neonPolice" @click="$router.push('chat')">Chat &#128062;</v-btn>
 				</div>
-				<div class="box one">Box 1 &#128001</div>
-				<div class="box two">Box 2 &#128048</div>
-				<div class="box three">Box 3 &#129418</div>
-				<div class="box four">Box 4 &#128013</div>
-				<div class="box five">Box 5 &#128014</div>
-				<div class="box six">Box 6 &#128025</div>
-				<div class="box seven">Box 7 &#128029</div>
-				<div class="box eight">Box 8 &#128030</div>
-				<div class="box nine">Box 9 &#128060</div>
+/				<div class="box seven">Security &#128029
+					<v-btn class="neonPolice">Enable 2FA</v-btn>
+				</div>
+				<div class="box eight">Privacy &#128030
+					<v-btn class="neonPolice">Delete account ❌</v-btn>
+				</div>
+				<div class="box nine">Match history &#128060</div>
+				<div class="box ten">
+					<v-btn class="chatButtom neonPolice" @click="$router.push('chat')">Play ▶️ &#129513;</v-btn>
+				</div>
 
 			</v-container>
 		</section>
@@ -211,50 +232,52 @@ export default defineComponent({
 .profileHome{
 	display:grid;
 	grid-template-columns: 1fr 1fr 1fr 1fr;
-	grid-template-rows: 100px 100px 100px 100px 100px;
+	grid-template-rows: 70px 70px 70px 70px 70px 70px 70px 70px 70px 70px 70px 70px;
 	gap: 10px;
 }
 
 .box {
 	background-color: darkgreen;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-}
-.zero {
-	grid-column: 1 / 2;
-	grid-row: 1 / 6;
+	/* display: flex; */
+	/* align-items: center; */
 }
 .one {
-	grid-column: 2 / 7;
+	grid-row: 1 / 8;
+	display: block;
 }
 .two {
 	/* grid-column: 2 / 3; */
-	grid-row: 2 / 4;
+	grid-row: 1 / 6;
+	display: block;
 }
 .three {
-	grid-column: 3 / 4;
+	grid-row: 1 / 6;
 }
 .four {
-	grid-column: 4 / 7;
+	grid-row: 1 / 6;
+	display: block;
 }
 .five {
-	grid-column: 3 / 7;
+	grid-column: 2 / 5;
+	grid-row: 6 / 9;
+	display: block;
 }
 .six {
-	grid-column: 2 / 4;
+	grid-row: 8 / 10;
 }
 .seven {
-	grid-column: 4 / 7;
-	background-color: green;
+	grid-row: 10 / 11;
 }
 .eight {
-	grid-column: 2 / 3;
-	background-color: green;
+	grid-row: 11 / 12;
 }
 .nine {
-	grid-column: 3 / 7;
-	background-color: green;
+	grid-column: 2 / 4;
+	grid-row: 9 / 12;
+}
+.ten {
+	grid-column: 4 / 5;;
+	grid-row: 9 / 12;
 }
 
 @media screen and (max-width: 960px) {
