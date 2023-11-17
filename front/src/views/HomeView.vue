976f2cdf7d310c6a0fb36c2c24e552c53b86d7bb
Theo ZERIBI <tzeribi@gmail.com>
2023-11-17 18:42:49 +0100

    chore: update all module dependencies

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
-	<hello-world />
+	<v-app id="home">
+		<NavBar>
+			<v-container fluid>
+				<div class="head">
+
+				</div>
+			</v-container>
+		</NavBar>
+	</v-app>
 </template>
 
 <script lang="ts">
@@ -14,3 +22,28 @@ export default defineComponent({
 	components: { HelloWorld },
 });
 </script>
+
+<style scoped>
+.v-container{
+	padding: 16px 0;
+}
+.head{
+	position: relative;
+	text-align: center;
+	padding: 12px;
+	margin-bottom: 6px;
+	height: 400px;
+	width: 100%;
+	color: white;
+}
+.head:before{
+	content: " ";
+	position: absolute;
+	top: 0;
+	left: 0;
+	height: 100%;
+	width: 50%;
+	background: black;
+	transform: skew(0deg, 6deg);
+}
+</style>
