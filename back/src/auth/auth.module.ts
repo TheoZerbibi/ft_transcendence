755e71428116dba9dd128e2bb0994d6abd3302b3
Theo ZERIBI <tzeribi@gmail.com>
2023-11-17 20:20:54 +0100

    fix(back): Revert Auth module

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-/* import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -8,27 +8,5 @@ import { JwtStrategy } from './strategy';
 	controllers: [AuthController],
 	imports: [JwtModule.register({})],
 	providers: [AuthService, JwtStrategy],
-})
-export class AuthModule {}
- */
-// auth.module.ts
-import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { AuthService } from './auth.service';
-import { JwtStrategy } from './strategy/jwt.strategy';
-import { AuthController } from './auth.controller';
-
-@Module({
-	imports: [
-		PassportModule.register({ defaultStrategy: 'jwt' }),
-		JwtModule.register({
-			secret: process.env.JWT_SECRET, // Replace with your JWT_SECRET
-			signOptions: { expiresIn: '1h' }, // Adjust token expiration as needed
-		}),
-	],
-	controllers: [AuthController],
-	providers: [AuthService, JwtStrategy],
-	exports: [PassportModule, JwtModule],
 })
 export class AuthModule {}
