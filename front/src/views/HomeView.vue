601cc87ae66574281c590ca3b2f4a2b8ad6d105e
Theo ZERIBI <tzeribi@gmail.com>
2023-11-17 18:44:02 +0100

    chore: Fix front/src/plugins/vuetify.ts

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -8,7 +8,7 @@
 							<div style="position: relative" clas="mt-16">
 								<h1 class="text-grey">Hello,</h1>
 								<h1 class="text-grey">Welcome to ft_transcendence of</h1>
-								<span class="text-grey">Théo, Gaëlle, Semiha, Noémie and ?</span><br />
+								<span class="text-grey">Théo, Gaëlle, Semiha, Noémie and Norminet</span><br />
 								<v-btn title dark class="text-yellow mt-8" variant="outlined"></v-btn>
 							</div>
 						</v-col>
