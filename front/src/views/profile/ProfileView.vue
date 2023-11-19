444e0074fcd7af9645bb033c7ec34873669d4d46
Kyubie <gnouny@yahoo.fr>
2023-11-19 20:11:21 +0100

    border gradiant in profile page

--- front/src/views/profile/ProfileView.vue
+++ front/src/views/profile/ProfileView.vue
@@ -36,7 +36,7 @@
 					>
 				</div>
 				<div class="box seven">
-					Security;
+					Security
 					<v-btn class="customButton darkNeonPolice" rounded>Enable 2FA &#128506;</v-btn>
 				</div>
 				<div class="box eight">
@@ -72,7 +72,6 @@ export default defineComponent({
 	gap: 10px;
 	/* background-image: url(/src/assets/OmoriWallpapers/OmoriJournal01.png); */
 	/* background-size: cover; */
-	/* background: url(/src/assets/OmoriWallpapers/OmoriJournal01.png) no-repeat; */
 }
 
 .box {
@@ -85,6 +84,8 @@ export default defineComponent({
 	border: 2px solid #333; /* Couleur de la bordure et épaisseur */
 	padding: 10px; /* Ajoutez un espace intérieur pour rendre la bordure plus visible */
 	text-align: center;
+	border: 5px solid transparent;
+  	border-image: linear-gradient(to right, #470979, #00d4ff) 5 repeat;
 }
 .one {
 	grid-row: 1 / 6;
