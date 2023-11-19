e2961b4e3f19d66c15b15a32da5ae62a4b9be831
Semiha Beyazkilic <seozcan@student.42.fr>
2023-11-19 16:12:51 +0100

    feat: plugin makeId to create state token

---
+++ front/src/plugins/makeId.ts
@@ -0,0 +1,16 @@
+/**
+ * Generates a random string of the specified length.
+ * @param {number} length The length of the random string.
+ * @returns {string} The random string.
+ */
+export function makeid(length: number): string {
+	let result = '';
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	const charactersLength = characters.length;
+	let counter = 0;
+	while (counter < length) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		counter += 1;
+	}
+	return result;
+}
