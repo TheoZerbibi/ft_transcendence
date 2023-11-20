d6b547a14cdb974d608e704f6c00b1fffa3ee509
Kyubie <gnouny@yahoo.fr>
2023-11-20 21:10:48 +0100

    core:feat clean files + add /signin + home to make

--- front/src/components/utils/Footer.vue
+++ front/src/components/utils/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-	<v-footer class="neonPolice footer blurred-card" alt="Footer">
+	<v-footer class="darkNeonPolice footer blurred-card" alt="Footer">
 		<span style="color: black"> ☆⋆｡𖦹°‧★ </span>
-		<a href="https://www.instagram.com/42.born2code/" target="_blank">Instagram</a>
+		<a class="darkNeonPolice" href="https://www.instagram.com/42.born2code/" target="_blank">Instagram</a>
 		<span style="color: black"> 👁 </span>
-		<a href="https://www.facebook.com/42born2code" target="_blank">Facebook</a>
+		<a class="darkNeonPolice" href="https://www.facebook.com/42born2code" target="_blank">Facebook</a>
 		<span style="color: black"> 👁 </span>
-		<a href="https://twitter.com/42born2code?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"
+		<a class="darkNeonPolice" href="https://twitter.com/42born2code?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"
 			>Twitter</a
 		>
 		<span style="color: black"> ☆⋆｡𖦹°‧★ </span>
@@ -19,3 +19,18 @@
 		name: 'Footer',
 	});
 </script>
+
+<style scoped>
+.footer {
+	display: block;
+	position: absolute;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	padding-top: 14px;
+	padding-bottom: 14px;
+	padding-left: 24px;
+	height: 36px;
+	text-align: center;
+}
+</style>
