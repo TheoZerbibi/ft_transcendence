4ca69fb5c480b35604d607c650d1bf2ca022e1f4
Theo ZERIBI <tzeribi@gmail.com>
2023-11-20 23:28:14 +0100

    fix: merge conflict

--- front/src/components/chat/Toolbar.vue
+++ front/src/components/chat/Toolbar.vue
@@ -1,12 +1,32 @@
 <template>
-	<div class="toolbar">
-	  <!-- Éléments de la barre d'outils -->
-	</div>
+  <div class="Toolbar">
+	<v-app-bar>
+		<v-responsive max-width="156">
+			<v-text-field
+				bg-color="grey-lighten-1"
+				density="compact"
+				flat
+				hide-details
+				rounded="pill"
+				variant="solo-filled"
+			></v-text-field>
+		</v-responsive>
+	</v-app-bar>
+  </div>
 </template>
-  
-<script lang="ts">
-  export default {
-	name: 'Toolbar',
-	// ...
-  };
+
+<script>
+
+export default {
+  name: 'Toolbar',
+  date() {
+	return {
+	  // Données spécifiques à la toolbar
+	};
+  },
+  methods: {
+	// Autres méthodes spécifiques à la toolbar
+  },
+  // ...
+};
 </script>
