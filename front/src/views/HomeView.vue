32409368ad1087a8ebb7c102903bf34df5dbb722
Kyubie <gnouny@yahoo.fr>
2023-11-20 21:18:32 +0100

    core:feat clean HomeView.vue

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -11,26 +11,6 @@ import { makeid } from '../plugins/makeId';
 export default {
 	name: 'HomeView',
 	components: { Footer },
-	methods: {
-		redirectToOAuth() {
-			const clientId = import.meta.env.VITE_API42_UID;
-			const redirectUri = import.meta.env.VITE_API42_CALLBACK;
-			const responseType = 'code';
-			const scope = 'public';
-			const state = makeid(42);
-
-			const params = new URLSearchParams();
-			params.append('client_id', clientId);
-			params.append('redirect_uri', redirectUri);
-			params.append('response_type', responseType);
-			params.append('scope', scope);
-			params.append('state', state);
-
-			const query = params.toString();
-
-			window.location.href = `https://api.intra.42.fr/oauth/authorize?${query}`;
-		},
-	},
 };
 </script>
 
