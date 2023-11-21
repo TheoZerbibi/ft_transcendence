46980b1e64f52e6b1f173d57ee82299458a071d8
Theo ZERIBI <tzeribi@gmail.com>
2023-11-21 20:53:00 +0100

    feat(front): Rework login page

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,38 +1,24 @@
 <template>
-	<v-row align="center" justify="center" class="fill-height" id="background">
-		<v-card class="homepage text-center blurred-card rounded-card" variant="tonal" width="600">
-			<v-card-item>
-				<v-card-title class="title">
-					<p class="neonPolice">Welcome to NSTG's ft_transcendence</p>
-				</v-card-title>
-				<v-img
-					class="rounded-card"
-					src="/src/assets/OmoriPictures/Omori007.jpg"
-					contain
-					max-height="300"
-					alt="These are us"
-				/>
-				<v-card-subtitle class="neonPolice homeSubtitles">
-					<br />Gaelle, <span class="teamLeader">Theo</span>, Semiha, Noe
-				</v-card-subtitle>
-			</v-card-item>
+	<v-row align="center" justify="center" class="fill-height background d-flex" id="bg">
+		<img src="/ui/Door.png" class="polaroid" alt="These are us" @click="redirectToOAuth" id="polaroid" />
+		<!-- <div class="homepage text-center d-flex">
+			 <v-card-item>
+					<v-card-title class="title">
+						<p class="neonPolice">Welcome to NSTG's ft_transcendence</p>
+					</v-card-title>
+					<v-card-subtitle class="neonPolice homeSubtitles">
+						<br />Gaelle, <span class="teamLeader">Theo</span>, Semiha, Noe
+					</v-card-subtitle>
+				</v-card-item>
 			<v-card-text class="neonPolice text homeSubtitles"> <br />students of 42 paris<br /> </v-card-text>
 			<v-card-actions>
-				<v-btn
-					id="authButton"
-					class="text-capitalize"
-					rounded="xl"
-					color="#C306DF"
-					variant="outlined"
-					size="x-large"
-					@click="redirectToOAuth"
-				>
+				<v-btn id="authButton" class="text-capitalize" size="x-large" @click="redirectToOAuth">
 					<span class="neonPolice"> 💡 Sign in with 42 </span>
 				</v-btn>
 			</v-card-actions>
-		</v-card>
+		</div> -->
 	</v-row>
-	<Footer />
+	<!-- <Footer /> -->
 </template>
 
 <script lang="ts">
@@ -53,15 +39,15 @@ export default {
 </script>
 
 <style>
-#background {
-	background-image: url('/src/assets/OmoriHomepageWallpaper.jpg');
+.background {
+	background-color: white;
+	pointer-events: none;
 }
 
 .homepage {
 	display: flex;
 	align-items: center;
 	flex-direction: column;
-	/* background: linear-gradient(90deg, #8360c3, #2ebf91); */
 }
 
 .title {
@@ -76,10 +62,37 @@ export default {
 	color: darkmagenta;
 }
 
+.polaroid {
+	height: 40vw;
+	z-index: 99999999;
+	pointer-events: auto;
+}
+
+.polaroid:hover {
+	-webkit-filter: brightness(10);
+	filter: brightness(10);
+	filter: invert(1);
+	cursor: url(https://www.omori-game.com/img/cursor/cursor.png), auto;
+	-webkit-box-shadow: 0 4px 4px -2px #000000;
+	-moz-box-shadow: 0 4px 4px -2px #000000;
+	box-shadow: 0 40px 40px -2px #000000;
+	transition: 0.5s ease-in-out all;
+}
+
+.background:hover {
+	background-color: black;
+	transition: 1s ease-in-out all;
+}
+
+.group-image {
+	height: 100%;
+	width: 100%;
+	z-index: -99999999;
+}
+
 #authButton:hover {
 	background-color: black;
 	box-shadow: 6px 6px 0px rgba(0, 0, 0);
-	/* box-shadow: 6px 6px 25px rgba(0, 0, 0, 0.5); */
 	border: double;
 }
 
