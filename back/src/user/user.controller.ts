6269d6768426e050be0d3814784681944fbfa099
Theo ZERIBI <tzeribi@gmail.com>
2023-11-22 18:17:54 +0100

    feat(back): Start implementing Cloudinary

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,12 +1,16 @@
+/* eslint-disable indent */
 import {
 	BadRequestException,
 	Body,
 	ClassSerializerInterceptor,
 	Controller,
+	FileTypeValidator,
 	Get,
 	Param,
+	ParseFilePipe,
 	Patch,
 	Post,
+	UploadedFile,
 	UseGuards,
 	UseInterceptors,
 } from '@nestjs/common';
@@ -17,6 +21,10 @@ import { EditUserDto, UserDto } from './dto';
 import { JwtGuard } from 'src/auth/guard';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { Express } from 'express';
+import { multerOptions } from './multer/multer.config';
 
 @Controller('users')
 @ApiBearerAuth()
@@ -41,6 +49,16 @@ export class UserController {
 		const user: UserDto | undefined = await this.userService.getUserByLogin(userLogin);
 		if (!user) throw new BadRequestException('Invalid user');
 		return user;
+	}
+
+	@UseGuards(JwtGuard)
+	@Post('getCloudinaryLink')
+	@ApiOperation({ summary: 'Get Avatar Link from Cloudinary API' })
+	@ApiBearerAuth('JWT-auth')
+	@UseInterceptors(FileInterceptor('file', multerOptions))
+	async getLink(@UploadedFile() file: Express.Multer.File): Promise<string> {
+		console.log(file);
+		return 'null';
 	}
 
 	@Post()
