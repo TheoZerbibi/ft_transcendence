6c56427dafb51a7934f1474d59c7e07f16bf1cf0
Theo ZERIBI <tzeribi@gmail.com>
2023-11-22 14:40:24 +0100

    feat(back): Start implementing Cloudinary

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,32 +1,13 @@
 <template>
 	<v-row align="center" justify="center" class="fill-height background d-flex" id="bg">
-		<img src="/ui/Door.png" class="polaroid" alt="These are us" @click="redirectToOAuth" id="polaroid" />
-		<!-- <div class="homepage text-center d-flex">
-			 <v-card-item>
-					<v-card-title class="title">
-						<p class="neonPolice">Welcome to NSTG's ft_transcendence</p>
-					</v-card-title>
-					<v-card-subtitle class="neonPolice homeSubtitles">
-						<br />Gaelle, <span class="teamLeader">Theo</span>, Semiha, Noe
-					</v-card-subtitle>
-				</v-card-item>
-			<v-card-text class="neonPolice text homeSubtitles"> <br />students of 42 paris<br /> </v-card-text>
-			<v-card-actions>
-				<v-btn id="authButton" class="text-capitalize" size="x-large" @click="redirectToOAuth">
-					<span class="neonPolice"> 💡 Sign in with 42 </span>
-				</v-btn>
-			</v-card-actions>
-		</div> -->
+		<img src="/ui/Door.png" class="door" alt="These are us" @click="redirectToOAuth" id="polaroid" />
 	</v-row>
-	<!-- <Footer /> -->
 </template>
 
 <script lang="ts">
-import Footer from '../components/layout/Footer.vue';
 
 export default {
 	name: 'SignIn',
-	components: { Footer },
 	methods: {
 		redirectToOAuth() {
 			const HOST = import.meta.env.VITE_HOST;
@@ -44,31 +25,13 @@ export default {
 	pointer-events: none;
 }
 
-.homepage {
-	display: flex;
-	align-items: center;
-	flex-direction: column;
-}
-
-.title {
-	display: flex;
-	align-items: flex-start;
-	margin: 20px;
-	padding: 20px;
-}
-
-.teamLeader {
-	font-weight: bold;
-	color: darkmagenta;
-}
-
-.polaroid {
+.door {
 	height: 40vw;
 	z-index: 99999999;
 	pointer-events: auto;
 }
 
-.polaroid:hover {
+.door:hover {
 	-webkit-filter: brightness(10);
 	filter: brightness(10);
 	filter: invert(1);
@@ -84,19 +47,4 @@ export default {
 	transition: 1s ease-in-out all;
 }
 
-.group-image {
-	height: 100%;
-	width: 100%;
-	z-index: -99999999;
-}
-
-#authButton:hover {
-	background-color: black;
-	box-shadow: 6px 6px 0px rgba(0, 0, 0);
-	border: double;
-}
-
-#authButton:hover span {
-	color: white !important;
-}
 </style>
