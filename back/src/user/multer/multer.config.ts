64755972a29611871f757db05246b5d510ecd64c
Theo ZERIBI <tzeribi@gmail.com>
2023-11-23 15:36:49 +0100

    fix(Merge): Rebase

--- back/src/user/multer/multer.config.ts
+++ back/src/user/multer/multer.config.ts
@@ -5,13 +5,12 @@ import { v4 as uuid } from 'uuid';
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export const multerConfig = {
-	dest: '/uploads',
+	dest: './uploads',
 };
 
 export const multerOptions = {
 	fileFilter: (req: any, file: any, cb: any) => {
 		if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
-			console.log('file accepted');
 			cb(null, true);
 		} else {
 			cb(new HttpException(`Unsupported file type ${extname(file.originalname)}`, HttpStatus.BAD_REQUEST), false);
