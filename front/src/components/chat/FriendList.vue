cc3f1af7a554c4d1c49b1b83e455ee73bdc816d4
noe <nfauconn@student.42.fr>
2023-11-29 19:08:14 +0100

    fix: list of friend requests

--- front/src/components/chat/FriendList.vue
+++ front/src/components/chat/FriendList.vue
@@ -3,8 +3,9 @@
 		<h2>Friend Requests</h2>
 		<ul v-if="friendRequests.length">
 			<li v-for="request in friendRequests" :key="request.id">
-				{{ request.username }}
+				{{ request.author_username }} <!-- L'objet contient aussi l'avatar si tu veux -->
 				<button @click="acceptFriendRequest(request.id)">Accept</button>
+				<span>&nbsp;</span>
 				<button @click="declineFriendRequest(request.id)">Decline</button>
 			</li>
 		</ul>
@@ -21,9 +22,19 @@ import { useUser } from '../../stores/user';
 import { useSocketStore } from '../../stores/websocket';
 import Snackbar from '../layout/Snackbar.vue';
 
+const userStore = useUser();
 const snackbarStore = useSnackbarStore();
 
 export default {
+	setup() {
+		const JWT = computed(() => userStore.getJWT);
+		const user = computed(() => userStore.getUser);
+
+		return {
+			JWT,
+			user,
+		};
+	},
 	data() {
 		return {
 			friendRequests: []
@@ -34,29 +45,30 @@ export default {
 	},
 	mounted() {},
 	methods: {
-	fetchFriendRequests: async function() {
-		try {
-			const route = useRoute();
-			const response = await fetch(
-				`http://${import.meta.env.VITE_HOST}:${import.meta.env.VITE_API_PORT}/user/friends/requests`,
-				{
-					method: 'GET',
-					headers: {
-						Authorization: `Bearer ${this.JWT}`,
-						'Access-Control-Allow-Origin': '*',
-					},
+		fetchFriendRequests: async function() {
+			try {
+				const route = useRoute();
+				const response = await fetch(
+					`http://${import.meta.env.VITE_HOST}:${import.meta.env.VITE_API_PORT}/users/friends/requests`,
+					{
+						method: 'GET',
+						headers: {
+							Authorization: `Bearer ${this.JWT}`,
+							'Access-Control-Allow-Origin': '*',
+						},
+					}
+				);
+				if (!response.ok) {
+					console.error('error');
+					// TODO
 				}
-			);
-			if (!response.ok) {
-				console.error('error');
-				// TODO
+				const data = await response.json();
+				this.friendRequests = data;
+				console.log(data);
+			} catch (error) {
+				console.log(error);
 			}
-			const data = await response.json();
-			this.friendRequests = data;
-		} catch (error) {
-			console.log(error);
-		}
-	},
+		},
 	}
 }
 </script>
