aff91987f574816aea2c7515ea1e3fb6ffe4d17b
Semiha Beyazkilic <seozcan@student.42.fr>
2023-12-04 19:12:33 +0100

    feat: global css refact

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,8 +1,35 @@
 <template>
-	<v-row align="center" justify="center" class="fill-height" id="background">
-		<h1 class="darkNeonPolice">Bienvenue {{ user.displayName }} !</h1>
-	</v-row>
-	<Footer />
+	<v-main id="background">
+		<table>
+			<tr>
+				<th>omoriFont</th>
+				<th>omoriDisturbed</th>
+				<th>Arcade</th>
+			</tr>
+			<tr>
+				<td><h1 class="omoriFont">Titre h1</h1></td>
+				<td><h1 class="omoriDisturbed">Titre h1</h1></td>
+				<td><h1 class="omoriArcade">Titre h1</h1></td>
+			</tr>
+			<tr>
+				<td><h2 class="omoriFont">Titre h2</h2></td>
+				<td><h2 class="omoriDisturbed">Titre h2</h2></td>
+				<td><h2 class="omoriArcade">Titre h2</h2></td>
+			</tr>
+			<tr>
+				<td><h3 class="omoriFont">Titre h3</h3></td>
+				<td><h3 class="omoriDisturbed">Titre h3</h3></td>
+				<td><h3 class="omoriArcade">Titre h3</h3></td>
+			</tr>
+			<tr>
+				<td><p class="omoriFont">Paragraphe</p></td>
+				<td><p class="omoriDisturbed">Paragraphe</p></td>
+				<td><p class="omoriArcade">Paragraphe</p></td>
+			</tr>
+		</table>
+		
+		
+	</v-main>
 </template>
 
 <script lang="ts">
@@ -18,7 +45,7 @@ export default {
 	setup() {
 		const JWT = computed(() => userStore.getJWT);
 		const user = computed(() => userStore.getUser);
-
+		
 		return {
 			JWT,
 			user,
@@ -42,8 +69,25 @@ export default {
 
 <style>
 #background {
-	background-image: url('/src/assets/OmoriWallpapers/OmoriHeadSpace.png');
+	/* background-image: url('/src/assets/OmoriWallpapers/OmoriHeadSpace.png'); */
 	background-repeat: no-repeat;
 	background-position: center;
 }
+
+table, th, td {
+	border: 1px solid white;
+    white-space: nowrap;
+	border-collapse: collapse;
+	padding: auto;
+}
+
+table {
+	width: 70%;
+}
+
+th, td {
+	height: auto;
+	align-items: center;
+}
+
 </style>
