8fc82755a33d3cd8f1438cb4f2dc65e9d613fcf2
Semiha Beyazkilic <seozcan@student.42.fr>
2023-12-05 20:08:41 +0100

    feat: buttons and boxes

---
+++ front/src/components/layout/Box.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="box" :style="{ width: width, height: height }">
+		<div class="rectangle">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default {
+	name: "Box",
+	props: {
+		width: {
+			type: String,
+			default: "100px",
+		},
+		height: {
+			type: String,
+			default: "50px",
+		},
+	}
+};
+</script>
+
+<style>
+.box .rectangle {
+	background-color: #000000;
+	border-color: #ffffff;
+	border: 3px solid;
+	margin: 16px;
+	padding: 6px;
+ 	display: flex;
+	position: relative;
+}
+
+.box-container {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-evenly;
+}
+</style>
