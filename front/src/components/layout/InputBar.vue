704108b655a504a01d0b57c9f55bf1b87f32b04c
Semiha Beyazkilic <seozcan@student.42.fr>
2023-12-07 21:05:43 +0100

    feat: login rework

--- front/src/components/layout/InputBar.vue
+++ front/src/components/layout/InputBar.vue
@@ -1,50 +1,44 @@
 <template>
-<input 
-type="text"
-name="text"
-class="input omoriPolice"
-placeholder="Type your text" 
-v-model="input">
+	<input
+		type="text"
+		class="white--text omoriFont font-weight-black uppercase-placeholder"
+		:placeholder="placeholder"
+		@input="handleInput"
+		style="{
+			background-color: #000000;
+			border-color: #ffffff;
+			border: 3px solid;
+			padding: 6px;
+			position: relative;
+			font-size: 20px;
+			height: 50px;
+			width: 250px;
+		}"
+	/>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
 
-export default {
-	name: 'InputBar',
-	data() {
-		return {
-			input: '',
-		};
+export default defineComponent({
+	name: "InputBar",
+	props: {
+		placeholder: {
+			type: String,
+			default: 'Input'
+		},
 	},
-};
+	methods: {
+		handleInput(event: any) {
+			const input = event.target.value;
+			this.$emit("newInput", input);
+		},
+	},
+});
 </script>
 
 <style scoped>
-.input {
- background-color: #383838;
- border: 1ex solid none;
- border-top-width: 1.7em;
- margin: 10px;
- padding: 0;
- color: #383838;
- word-wrap: break-word;
- outline: 7px solid #383838;
- height: 30px;
- font-size: 17px;
- text-align: center;
- -webkit-transition: all 1s;
- transition: all 1s;
- max-width: 100%;
- font-weight: bold;
-}
-
-.input:hover {
- border-top-width: 0.2em;
- background-color: #f1e8e8;
-}
-
-.input:focus {
- border-top-width: 0.2em;
- background-color: #f1e8e8;
+.uppercase-placeholder::placeholder {
+    text-transform: uppercase;
 }
 </style>
