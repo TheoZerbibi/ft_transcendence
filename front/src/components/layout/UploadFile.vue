97e5b5bdf8ea142495da563458dc6176330e8874
Theo ZERIBI <tzeribi@gmail.com>
2023-12-11 16:12:45 +0100

    fix(Merge): Rebase

--- front/src/components/layout/UploadFile.vue
+++ front/src/components/layout/UploadFile.vue
@@ -1,79 +1,79 @@
 <template>
-	<form class="file-upload-form">
-		<label for="file" class="file-upload-label">
-			<div class="file-upload-design">
-				<svg viewBox="0 0 640 512" height="1em">
-					<path
-						d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"
-					></path>
-				</svg>
-				<p>DRAG AND DROP</p>
-				<p>or</p>
-				<span class="browse-button">BROWSE</span>
-			</div>
-			<input id="file" type="file" @change="handleFileChange" />
-		</label>
+	<form id="polaroid">
+		<div style="
+			justify-content: center;
+			align-items: center;
+			display: flow-root;
+			position: relative;
+			width: 220px;
+			height: 220px;
+			top: -23px;"
+		>
+			<slot name="image" class-="slot-image"></slot>
+			<label for="file" class="file-upload-label hoverable">
+				<img src="../../../public/login/upload.svg" class="file-upload-design hoverable" alt="image" />
+				<input id="file" type="file" @change="handleFileChange" />
+			</label>
+		</div>
 	</form>
 </template>
 
 <script lang="ts">
-export default {
-	methods: {
-		handleFileChange(event: any) {
-			const file = event.target.files[0];
-			this.$emit('imageChanged', file);
-		},
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent ({
+	name: 'UploadFile',
+	setup(props, { emit }) {
+		const file = ref("")
+
+		function handleFileChange(event: any) {
+			file.value = event.target.files[0];
+			emit('imageChanged', file.value);
+		}
+
+		return {
+			file,
+			handleFileChange
+		};
 	},
-};
+});
 </script>
 
 <style scoped>
-p {
-	font-family: 'OMORI_MAIN', sans-serif;
-	font-size: 20px;
-	color: rgb(255, 255, 255);
-	margin: 5px;
+
+#polaroid {
+	background-image: url('/public/ui/Polaroid.svg');
+ 	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+	aspect-ratio: 1;
+	position: relative;
+	height: inherit;
+	width: inherit;
+	margin: auto;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 0;
 }
 
-.file-upload-form {
-	width: fit-content;
-	height: fit-content;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-}
+
 .file-upload-label input {
 	display: none;
 }
-.file-upload-label svg {
-	height: 50px;
-	fill: rgb(82, 82, 82);
-	margin-bottom: 20px;
-}
-.file-upload-label {
-	cursor: pointer;
-	background-color: #ddd;
-	padding: 30px 70px;
-	border-radius: 40px;
-	border: 2px dashed rgb(82, 82, 82);
-	box-shadow: 0px 0px 200px -50px rgba(0, 0, 0, 0.719);
-}
+
 .file-upload-design {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	gap: 5px;
+	object-fit: cover;
+	opacity: 0;
+	transition: opacity 0.3s ease;
 }
-.browse-button {
-	background-color: rgb(82, 82, 82);
-	padding: 5px 15px;
-	border-radius: 10px;
-	color: white;
-	transition: all 0.3s;
-	font-family: 'OMORI_MAIN', sans-serif;
+
+.slot-image {
+	object-fit: cover;
 }
-.browse-button:hover {
-	background-color: rgb(14, 14, 14);
+
+.file-upload-design:hover {
+	opacity: 0.8;
 }
+
 </style>
