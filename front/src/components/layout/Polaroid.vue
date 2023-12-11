44eb17489b05e8d060550483494be2163044566d
Semiha Beyazkilic <seozcan@student.42.fr>
2023-12-11 22:09:49 +0100

    feat: avatar view finished

---
+++ front/src/components/layout/Polaroid.vue
@@ -0,0 +1,49 @@
+<template>
+	<div 
+		id="polaroid"
+		align="center"
+		justify="center"
+		:style="{ width: width + 'dvh', height: height + 'dvh'}"
+	>
+		<slot name="polaroidImg" class="slot-image"></slot>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'Polaroid',
+	props: {
+		width: {
+			type: String,
+			default: "25dvh",
+		},
+		height: {
+			type: String,
+			default: "30dvh",
+		},
+	},
+});
+</script>
+
+<style scoped>
+#polaroid {
+	background-image: url('/public/ui/polaroid.png');
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+	aspect-ratio: 1;
+	position: relative;
+	margin: auto;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 1;
+}
+
+.slot-image {
+	object-fit: cover;
+	z-index: 0;
+}
+</style>
--- front/src/components/layout/UploadFile.vue
+++ front/src/components/layout/UploadFile.vue
@@ -1,33 +1,54 @@
 <template>
-	<form id="polaroid">
-		<div style="
-			justify-content: center;
-			align-items: center;
-			display: flow-root;
-			position: relative;
-			width: 220px;
-			height: 220px;
-			top: -23px;"
-		>
-			<slot name="image" class-="slot-image"></slot>
-			<label for="file" class="file-upload-label hoverable">
-				<img src="../../../public/login/upload.svg" class="file-upload-design hoverable" alt="image" />
-				<input id="file" type="file" @change="handleFileChange" />
-			</label>
-		</div>
-	</form>
-	<!-- <Button @click="handleFileChange" :style="{ width: width + 'px', height: height + 'px'}">
-		<slot></slot>
-	</Button> -->
+	<div>
+		<form id="polaroid">
+			<div style="
+		  		width: 220px;
+		  		height: 220px;
+				position: relative;"
+			>
+				<label for="file" class="file-upload-label hoverable">
+					<div class="slot-image" 
+						@mouseover="showOverlay = true"
+						@mouseleave="showOverlay = false">
+						<slot name="polaroidImg"></slot>
+					</div>
+					<input id="file" type="file" @change="handleFileChange" />
+				</label>
+			</div>
+		</form>
+	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import Button from './Button.vue';
 
-export default defineComponent ({
+export default defineComponent({
 	name: 'UploadFile',
+	components: {
+		Button
+	},
+	props: {
+		buttonWidth: {
+			type: String,
+			default: '250px'
+		},
+		buttonHeight: {
+			type: String,
+			default: '50px'
+		},
+		polaroidWidth: {
+			type: String,
+			default: '25dvh'
+		},
+		polaroidHeight: {
+			type: String,
+			default: '30dvh'
+		},
+	},
 	setup(props, { emit }) {
 		const file = ref("")
+		const showOverlay = ref(false);
 
 		function handleFileChange(event: any) {
 			file.value = event.target.files[0];
@@ -36,47 +57,54 @@ export default defineComponent ({
 
 		return {
 			file,
-			handleFileChange
+			handleFileChange,
+			showOverlay,
 		};
 	},
 });
 </script>
 
 <style scoped>
-
 #polaroid {
-	background-image: url('/public/ui/Polaroid.svg');
- 	background-size: cover;
+	background-image: url('/public/ui/polaroid.png');
+	background-size: cover;
 	background-position: center;
 	background-repeat: no-repeat;
 	aspect-ratio: 1;
-	position: relative;
 	height: inherit;
 	width: inherit;
-	margin: auto;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	z-index: 0;
+	padding: 2dvh 2dvh 0dvh 2dvh;
+	z-index: 2;
 }
-
 
 .file-upload-label input {
 	display: none;
 }
 
-.file-upload-design {
+.slot-image {
+	display: flex;
+	align-items: center;
+	justify-content: center;
 	object-fit: cover;
+	top: 0;
+	left: 0;
+	z-index: 1;
+}
+
+.slot-image::before {
+	content: url('/public/login/upload.svg');
+	position: absolute;
+	top: 25%;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	z-index: 2;
 	opacity: 0;
 	transition: opacity 0.3s ease;
 }
 
-.slot-image {
-	object-fit: cover;
-}
-
-.file-upload-design:hover {
+.slot-image:hover::before {
 	opacity: 0.8;
 }
-
 </style>
