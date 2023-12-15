76fb3944c57f832b38b3fb2e1002a29a9e1b0f66
noe <nfauconn@student.42.fr>
2023-12-15 16:46:03 +0100

    feat: settings in a separate component. scrollable

---
+++ front/src/components/chat/channels/ChannelSettings.vue
@@ -0,0 +1,208 @@
+<template>
+
+	<!-- Channel Settings -->
+	<v-card class="scrollable-card" v-if="selectedChannelName">
+		<v-card-title> Settings </v-card-title>
+
+		<v-card-text>
+			<!-- Password modification -->
+			<v-text-field
+				v-model="pwd.prev"
+				label="Prev Password"
+				type="password"
+				max-length="20"
+			></v-text-field>
+			<v-text-field
+				v-model="pwd.new"
+				label="New Password"
+				type="password"
+				max-length="20"
+			></v-text-field>
+			<v-text-field
+				v-model="pwd.confirm"
+				label="Confirm Password"
+				type="password"
+				max-length="20"
+			></v-text-field>
+			<v-btn @click="changePassword">Change Password</v-btn>
+		</v-card-text>
+		<v-card-actions>
+			<v-btn @click="leaveChannel">Leave Channel</v-btn>
+			<v-btn @click="deleteChannel">Delete Channel</v-btn>
+		</v-card-actions>
+	</v-card>
+
+	<v-card v-else>
+		<v-card-title>Settings</v-card-title>
+		<v-card-text class="empty-card">
+			~ no channel selected ~
+		</v-card-text>
+	</v-card>
+
+
+
+	<!-- Error handling -->
+	<Snackbar></Snackbar>
+
+</template>
+
+
+<script lang="ts">
+import { computed, ref } from 'vue';
+import { useUser } from '../../../stores/user';
+import { useSnackbarStore } from '../../../stores/snackbar';
+import Snackbar from '../../layout/Snackbar.vue';
+
+const userStore = useUser();
+const snackbarStore = useSnackbarStore();
+
+export default {
+
+	components: {
+		Snackbar,
+	},
+	props: {
+		selectedChannelName: String
+	},
+	setup() {
+		const JWT = computed(() => userStore.getJWT);
+		const user = computed(() => userStore.getUser);
+
+		return {
+			JWT,
+			user,
+		};
+	},
+	data() {
+		return {
+			channelName: this.selectedChannelName ? this.selectedChannelName : '' as string,
+			pwd: {
+				prev: '' as string,
+				new: '' as string,
+				confirm: '' as string,
+			},
+		};
+	},
+	watch: {
+		selectedChannelName: function(newVal: string) {
+			this.channelName = newVal;
+		}
+	},
+	methods: {
+		leaveChannel: async function() {
+			try {
+				if (!this.channelName || this.channelName === '') {
+					console.log('[leaveChannel]: channelName is empty');
+					return;
+				}
+				const response: any = await fetch(
+					`http://${import.meta.env.VITE_HOST}:${import.meta.env.VITE_API_PORT}/channel/leave`,
+					{
+						method: 'DELETE',
+						headers: {
+							'Content-Type': 'application/json',
+							Authorization: `Bearer ${this.JWT}`,
+							'Access-Control-Allow-Origin': '*',
+						},
+						body: JSON.stringify({
+							name: this.channelName,
+						}),
+					}).catch((error: any) => {
+						snackbarStore.showSnackbar(error.message, 3000, 'red');
+						return;
+					});
+					const data: any = response.json();
+					if (data.is_error) {
+						snackbarStore.showSnackbar(data.error_message, 3000, 'red');
+						return;
+					}
+					if (!response.ok) {
+						snackbarStore.showSnackbar(response.message, 3000, 'red');
+						return;
+					}
+					snackbarStore.showSnackbar(`You left ${this.channelName}`, 3000, 'green');
+			} catch (error) {
+				console.error(error);
+			}
+		},
+		deleteChannel: async function() {
+			try {
+				if (!this.channelName || this.channelName === '') {
+					console.log('[deleteChannel]: channelName is empty');
+					return;
+				}
+				const response: any = await fetch(
+					`http://${import.meta.env.VITE_HOST}:${import.meta.env.VITE_API_PORT}/channel`,
+					{
+						method: 'DELETE',
+						headers: {
+							'Content-Type': 'application/json',
+							Authorization: `Bearer ${this.JWT}`,
+							'Access-Control-Allow-Origin': '*',
+						},
+						body: JSON.stringify({
+							name: this.channelName,
+						}),
+					}
+				).catch((error: any) => {
+					snackbarStore.showSnackbar(error.message, 3000, 'red');
+					return;
+				});
+				const data: any = response.json();
+				if (data.is_error) {
+					snackbarStore.showSnackbar(data.error_message, 3000, 'red');
+					return;
+				}
+				if (!response.ok) {
+					snackbarStore.showSnackbar(response.statusText, 3000, 'red');
+					return;
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		},
+		changePassword: async function() {
+			try {
+				if (!this.channelName || this.channelName === '') {
+					console.log('[changePassword]: channelName is empty');
+					return;
+				}
+				if (this.pwd.prev === '' && this.pwd.new === '' && this.pwd.confirm === '') {
+					return;
+				}
+				const response: any = await fetch(
+					`http://${import.meta.env.VITE_HOST}:${import.meta.env.VITE_API_PORT}/channel/${this.channelName}/settings/owner/pwd`,
+					{
+						method: 'PATCH',
+						headers: {
+							'Content-Type': 'application/json',
+							Authorization: `Bearer ${this.JWT}`,
+							'Access-Control-Allow-Origin': '*',
+						},
+						body: JSON.stringify({
+							prev_pwd: this.pwd.prev,
+							new_pwd: this.pwd.new,
+							new_pwd_confirm: this.pwd.confirm,
+						}),
+					}
+				).catch((error: any) => {
+					snackbarStore.showSnackbar(error.message, 3000, 'red');
+					return;
+				});
+				const data = await response.json();
+				if (data.is_error) {
+					snackbarStore.showSnackbar(data.error_message, 3000, 'red');
+					return;
+				}
+				if (!response.ok) {
+					snackbarStore.showSnackbar(response.statusText, 3000, 'red');
+					return;
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		},
+	},
+}
+
+</script>
