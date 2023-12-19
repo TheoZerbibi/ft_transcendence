102e49d7093dbc3e61cc3457dd3e9bbf4af4415b
Theo ZERIBI <tzeribi@gmail.com>
2023-12-19 02:22:38 +0100

    fix(Merge): Rebase

--- front/src/components/chat/direct-messages/DirectMessages.vue
+++ front/src/components/chat/direct-messages/DirectMessages.vue
@@ -7,7 +7,6 @@
 	<v-card>
 		<div v-if="selectedUserLogin && is_friend">
 			<v-card-text>
-
 				<!-- Chat Messages -->
 				<v-list>
 					<v-list-item v-for="message in messages" :key="message.id">
@@ -18,30 +17,35 @@
 						{{ message.content }}
 					</v-list-item>
 				</v-list>
-
 			</v-card-text>
 		</div>
 
 		<div v-else-if="selectedUserLogin">
-			<v-card-text >
-				~ you are not friend with this user so u cant chat ~>
-			</v-card-text>
+			<v-card-text> ~ you are not friend with this user so u cant chat ~> </v-card-text>
 		</div>
 
 		<div v-else>
 			<v-card-title>Messages</v-card-title>
-			<v-card-text>
-				~ no friend selected ~
-			</v-card-text>
+			<v-card-text> ~ no friend selected ~ </v-card-text>
 		</div>
-		<!-- Message Input -->		
+		<!-- Message Input -->
 	</v-card>
 	<v-footer rounded="0" elevation="0">
-		<v-text-field v-model="input" placeholder="Type your message..." max-length="200" variant="solo" rounded="0" flat
-			append-inner-icon="fas fa-paper-plane" @keyup.enter="sendMessage" @click:append-inner="sendMessage"
-			density="compact" clearable />
+		<v-text-field
+			v-model="input"
+			placeholder="Type your message..."
+			max-length="200"
+			variant="solo"
+			rounded="0"
+			flat
+			append-inner-icon="fas fa-paper-plane"
+			@keyup.enter="sendMessage"
+			@click:append-inner="sendMessage"
+			density="compact"
+			clearable
+		/>
 	</v-footer>
-	
+
 	<!-- Error handling -->
 	<Snackbar></Snackbar>
 </template>
@@ -54,7 +58,6 @@ import { useSnackbarStore } from '../../../stores/snackbar';
 import Snackbar from '../../layout/Snackbar.vue';
 import DateConv from '../../utils/DateConv.vue';
 
-import { inject } from 'vue';
 import { useSocketStore } from '../../../stores/websocket';
 
 const userStore = useUser();
@@ -69,39 +72,38 @@ export default {
 		const JWT = computed(() => userStore.getJWT);
 		const user = computed(() => userStore.getUser);
 
-		//	const webSocketStore = useSocketStore();
-		//
-		//	const socket = computed(() => webSocketStore.getSocket);
-		//	const isConnected = computed(() => webSocketStore.isConnected);
+		const webSocketStore = useSocketStore();
 
+		const socket = computed(() => webSocketStore.getSocket);
+		const isConnected = computed(() => webSocketStore.isConnected);
+		const connect = async (JWT: string) => {
+			await webSocketStore.connect(JWT, import.meta.env.VITE_CHAT_SOCKET_PORT);
+		};
 
 		return {
 			JWT,
 			user,
-			//			socket,
-			//			isConnected,
+			socket,
+			connect,
+			isConnected,
 		};
 	},
 	props: {
 		selectedUserLogin: String,
 		refresh: Number,
 	},
-	mounted() {
-		//		console.log(`[DirMsg-Socket] state: ${isConnected.value}`);
-		//	       this.socket.on('new-direct-message', (data) => {
-		//			       const msg: any = JSON.parse(data);
-		//			       if (msg !== undefined)
-		//			       console.log (`new-direct-msg - msg: ${msg.content}`);
-		//			       else
-		//				console.log('Error direct msg failed');
-		//	
-		//			       });
+	async mounted() {
+		await this.connect(this.JWT, import.meta.env.VITE_CHAT_SOCKET_PORT);
+		console.log(`[DirMsg-Socket] state: ${this.isConnected}`);
+		this.socket.on('new-direct-message', (data) => {
+			const msg: any = JSON.parse(data);
+			if (msg !== undefined) console.log(`new-direct-msg - msg: ${msg}`);
+			else console.log('Error direct msg failed');
+		});
 	},
 	data() {
 		return {
-			userLogin: this.selectedUserLogin ?
-				this.selectedUserLogin as string
-				: '' as string,
+			userLogin: this.selectedUserLogin ? (this.selectedUserLogin as string) : ('' as string),
 			messages: [] as any,
 			input: '' as string,
 			is_friend: false as boolean,
@@ -126,14 +128,16 @@ export default {
 
 				// Check if selected user is a friend
 				const isFriend: any = await fetch(
-					`http://${import.meta.env.VITE_HOST}:${import.meta.env.VITE_API_PORT}/users/friends/isfriend/${this.userLogin}`,
+					`http://${import.meta.env.VITE_HOST}:${import.meta.env.VITE_API_PORT}/users/friends/isfriend/${
+						this.userLogin
+					}`,
 					{
 						method: 'GET',
 						headers: {
 							Authorization: `Bearer ${this.JWT}`,
 							'Access-Control-Allow-Origin': '*',
 						},
-					}
+					},
 				);
 				if (!isFriend.ok) {
 					const error = await isFriend.json();
@@ -149,14 +153,16 @@ export default {
 
 				// Fetch messages
 				const response: any = await fetch(
-					`http://${import.meta.env.VITE_HOST}:${import.meta.env.VITE_API_PORT}/directMessage/${this.userLogin}/all`,
+					`http://${import.meta.env.VITE_HOST}:${import.meta.env.VITE_API_PORT}/directMessage/${
+						this.userLogin
+					}/all`,
 					{
 						method: 'GET',
 						headers: {
 							Authorization: `Bearer ${this.JWT}`,
 							'Access-Control-Allow-Origin': '*',
 						},
-					}
+					},
 				);
 				if (!response.ok) {
 					const error = await response.json();
@@ -166,7 +172,6 @@ export default {
 				const data = await response.json();
 
 				this.messages = data;
-
 			} catch (error: any) {
 				snackbarStore.showSnackbar(error, 3000, 'red');
 			}
@@ -174,8 +179,7 @@ export default {
 
 		sendMessage: async function () {
 			try {
-				if (!this.userLogin || this.userLogin === ''
-					|| this.input.trim() === '') {
+				if (!this.userLogin || this.userLogin === '' || this.input.trim() === '') {
 					return;
 				}
 				const response: any = await fetch(
@@ -191,7 +195,7 @@ export default {
 							target_login: this.userLogin,
 							content: this.input,
 						}),
-					}
+					},
 				);
 
 				if (!response.ok) {
@@ -204,14 +208,12 @@ export default {
 
 				this.fetchDirectMessages();
 				this.input = '';
-
 			} catch (error: any) {
 				snackbarStore.showSnackbar(error, 3000, 'red');
 			}
 		},
 	},
 };
-
 </script>
 
 <style scoped>
